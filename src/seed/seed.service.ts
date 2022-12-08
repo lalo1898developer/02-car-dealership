051daf18b1f3bd3fceded6efba941fbdf05f83d1
Eduardo Rodriguez Ricardez <lalo.work.tech@outlook.com>
2022-12-08 14:36:41 -0600

    Seccion 6-2: ejecutar comando para crear recurso seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { BRANDS_SEED } from './data/brands.seed';
+import { CARS_SEED } from './data/cars.seed';
+
+@Injectable()
+export class SeedService {
+  populateDB() {
+    // CARS_SEED
+    // BRANDS_SEED
+    return 'SEED executed successfull';
+  }
+}
